deff1233d0a88decec926775afff50cddd8360b6
Anna Nescierowicz <annie.odesk@gmail.com>
2022-04-22 17:58:10 +0300

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,24 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(!arguments.length || (typeof sampleActivity !== 'string')){
+    return false;
+  }
+
+  const sampleActivityNumber = parseFloat(sampleActivity);
+
+  if(isNaN(sampleActivityNumber) || sampleActivityNumber <= 0 || sampleActivityNumber > MODERN_ACTIVITY){
+    return false;
+  }
+
+  const LOG2 = 0.693;
+  const RATE = LOG2 / HALF_LIFE_PERIOD;
+
+  const years = (Math.log(MODERN_ACTIVITY / sampleActivityNumber)) / RATE;
+
+  return Math.ceil(years);
+
 }
 
 module.exports = {
