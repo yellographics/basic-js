4890d5b0660c9092388c1c430b2b202d4a4293b5
Anna Nescierowicz <annie.odesk@gmail.com>
2022-04-25 23:01:06 +0300

    feat: add solution for delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  // переводим число в строку и делаем из нее массив
+  const numberArray = String(n).split('');  
+
+  // создаем массив, в который будем запихивать массивы строк
+  const array = [];
+
+  // заводим цикл для сплайса, внутри добавляем временный массив
+  for (let i = 0; i < numberArray.length; i++){
+    let tempArray = [...numberArray];
+    tempArray.splice(i, 1);
+    console.log(tempArray)
+    array.push(tempArray);
+  }
+
+  for (let i = 0; i < array.length; i++){
+    array[i] = Number(array[i].join(''));
+  }
+  
+  
+  return Math.max(...array);
+
 }
 
 module.exports = {
