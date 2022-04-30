08c271324ad6a2e5ca2837c36cb8aed2c960c9fb
Anna Nescierowicz <annie.odesk@gmail.com>
2022-04-30 12:53:15 +0300

    feat: add solution for mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let newMatrix = JSON.parse(JSON.stringify(matrix));  
+  
+  for (let i = 0; i < matrix.length; i++){
+    for (let j = 0; j < matrix[i].length; j++){
+      newMatrix[i][j] = 0;
+    }
+  } 
+  
+  for (let i = 0; i < matrix.length; i++){
+    for (let j = 0; j < matrix[i].length; j++){
+      if(matrix[i][j] === true){        
+        if (matrix[i - 1]){          
+          if(matrix[i - 1][j - 1] !== undefined){            
+            newMatrix[i - 1][j - 1]++;
+          }
+          if(matrix[i - 1][j] !== undefined){            
+            newMatrix[i - 1][j]++;
+          }
+          if(matrix[i - 1][j+1] !== undefined){            
+            newMatrix[i - 1][j+1]++;
+          }
+        }
+        if (matrix[i][j - 1] !== undefined){
+          newMatrix[i][j - 1]++;
+        }
+        if(matrix[i][j + 1] !== undefined){
+          newMatrix[i][j + 1]++;
+        }
+        if (matrix[i + 1]){
+          if(matrix[i + 1][j-1] !== undefined){
+            newMatrix[i + 1][j-1]++;
+          }
+          if(matrix[i + 1][j] !== undefined){
+            newMatrix[i + 1][j]++;
+          }
+          if(matrix[i + 1][j+1] !== undefined){
+            newMatrix[i + 1][j+1]++;
+          }
+        }
+      }
+    }
+  }
+  
+  return newMatrix;
 }
 
 module.exports = {
